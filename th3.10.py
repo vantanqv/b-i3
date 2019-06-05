11d555883d9b89f8d681224f0ae880b3affad76f
vantanqv <49965682+vantanqv@users.noreply.github.com>
2019-06-05 13:52:48 +0700

    Add files via upload

---
+++ th3.10.py
@@ -0,0 +1,12 @@
+#tính diện tích, chu vi hình tròn
+import math 
+r=int(input("nhap ban kinh r"))
+def tinh(r):
+ n=r*2*3.14
+ s=3.14*r**2
+ print('',s)
+ print('',n)
+tinh(r)
+
+
+
---
+++ th3.11.py
@@ -0,0 +1,9 @@
+#tính lãi suất gửi ngân hàng 
+t=float(input("nhap lai suat t"))
+n=int(input("nhap so von ban dau"))
+k=int(input("nhap so thang k"))
+def benefit(t,n,k):
+    s=t*n*k
+    input(s)
+
+benefit(t,n,k)
